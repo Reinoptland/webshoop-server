826f911c5e8c871a82d6bb171b56e93361442de3
Reinoptland <reinoptland@gmail.com>
2020-03-20 17:31:11 +0100

    Made route for POST products

--- routers/products.js
+++ routers/products.js
@@ -4,11 +4,17 @@ const Product = require("../models").product;
 // registered as '/products'
 const router = new Router();
 
-// GET products
+// GET /products
 router.get("/", async (req, res) => {
   const allProducts = await Product.findAll();
   console.log("HI!");
   res.status(200).json(allProducts);
+});
+
+// POST /products
+router.post("/", (req, res) => {
+  console.log("HI!");
+  res.send("HELLO FROM BACKEND");
 });
 
 module.exports = router;
