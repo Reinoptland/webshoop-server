331fe34c3374235c54b0b0fbebef17decc13a918
Reinoptland <reinoptland@gmail.com>
2020-03-20 15:59:08 +0100

    Send response with products in GET products

--- routers/products.js
+++ routers/products.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
+const Product = require("../models").product;
 
 // registered as '/products'
 const router = new Router();
 
 // GET products
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const allProducts = await Product.findAll();
   console.log("HI!");
-  res.send("HI!");
+  res.status(200).json(allProducts);
 });
 
 module.exports = router;
